2edeb322e1e92f0a8294704ff547240c2ef8529a
lidåshuång <ldshuang@gmail.com>
2014-11-24 18:21:20 +0800

    Fixed

--- hello_world/lib/hello_world/supervisor.ex
+++ hello_world/lib/hello_world/supervisor.ex
@@ -2,12 +2,11 @@ defmodule HelloWorld.Supervisor do
   use Supervisor
 
   def start_link do
-    :supervisor.start_link({:local, __MODULE__}, __MODULE__, [])
-    # Supervisor.start_link(__MODULE__, [])
+    Supervisor.start_link(__MODULE__, [])
   end
 
   def init([]) do
-    procs = []
-    {:ok, {{:one_for_one, 10, 10}, procs}}
+    children = []
+    supervise(children, strategy: :one_for_one, max_restarts: 10, max_seconds: 10)
   end
 end
